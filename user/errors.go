4e9ffb506aceb9db2137e16a0c8b5bea70795e6d
rajkumar <rajkumar.bingi@joshsoftware.com>
2022-07-13 17:42:32 +0530

    added one custom error

--- user/errors.go
+++ user/errors.go
@@ -6,4 +6,6 @@ var (
 	errEmptyPassword = errors.New("User password must be presnet")
 	errEmptyName     = errors.New("User Name  must be presnet")
 	errNoUserId      = errors.New("User is not present")
+	invalidUserID    = errors.New("No user found")
+	errNoAccountId   = errors.New("User with ID not exist")
 )
